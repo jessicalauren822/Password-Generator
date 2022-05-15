f292964938d7572034d31dd17b8ed7cc2f3aaaaf
jessicawright822 <104212202+jessicawright822@users.noreply.github.com>
2022-05-14 20:20:41 -0400

    beginning of lowercase input details

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,51 @@
 // Assignment code here
-var passwordLength = function() {
-    passwordLength = (prompt("How many characters should the password have (between 8 - 128?"));
-  
-if (passwordLength >= 8 && passwordLength <=128){
-  
-}
+var password = function() {
+    passwordLength = (window.prompt("How many characters should the password have (between 8 - 128?"));
 
   if (passwordLength < 8 || passwordLength > 128) {
     window.alert("Password must be between 8 - 128 characters.");
-    return passwordLength();
+    return password();
   }
 
   if(passwordLength === "" || passwordLength === null) {
     window.alert("You need to provide a valid answer! Please try again.");
-    return passwordLength();
+    return password();
   }
-  
-  }
-passwordLength();
+    
+  if (passwordLength >= 8 && passwordLength <=128){
+    var lowercase = function () {
+      var lowerInput = window.prompt("Would you like your password to have lowercase letters? Please enter YES or NO.");
+      lowerInput = lowerInput.toLowerCase();
+      console.log(lowerInput);
 
-var lowercase = window.confirm("Would you like your password to have lowercase letters?");
-var uppercase = window.confirm("Would you like your password to have uppercase letters?");
-var numeric = window.confirm("Would you like your password to have numbers?");
-var special = window.confirm("Would you like your password to have special characters?");
+      if (lowerInput === "" || lowerInput === null) {
+       window.alert("You need to provide a valid answer! Please try again.");
+       
+     }
+     if (lowerInput === "yes" || lowerInput === "YES") {
+       return true; 
+     }
+     else if (lowerInput === "no" || lowerInput === "NO") {
+       return false;
+     }
+    }
+
+      var uppercase = function() {
+      window.prompt("Would you like your password to have uppercase letters? Please enter YES or NO.");
+    }
+
+      var numeric =  function() {
+        window.prompt("Would you like your password to have numbers? Please enter YES or NO.");
+        }
+
+      var special = function() {
+        window.prompt("Would you like your password to have special characters? Please enter YES or NO.");
+        }     
+
+  }
+}
+password();
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
